088ed29c6254579cbc2af11ed793a092cdbff41e
John Wallas <johnnes.wallas@gmail.com>
2021-07-28 23:42:16 -0300

    melhorado o metado de criar o arquivo

--- File_FileInfo/Program.cs
+++ File_FileInfo/Program.cs
@@ -1,48 +1,14 @@
 ﻿using System;
 using System.IO;
+using ProdutoV2.utilidade;
 namespace ProdutoV2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            try
-            {
-                StreamWriter arquivo;
-                string destino = @"C:\Produto.csv";
-                arquivo = File.CreateText(destino);
-                arquivo.Close();
-
-                char resp = 'S';
-                while (resp != 'N')
-                {
-                    Console.Write($"Nome do produto: ");
-                    string nome = Console.ReadLine();
-                    Console.Write($"Quantidade: ");
-                    int quant = int.Parse(Console.ReadLine());
-                    Console.Write($"Preço und do {nome}: ");
-                    double preco = double.Parse(Console.ReadLine());
-                    Console.Write($"Deseja cadastrar mais algum produto ? [S/N]");
-                    resp = char.Parse(Console.ReadLine().ToUpper());
-
-                    arquivo = File.AppendText(destino);
-                    arquivo.WriteLine($"{nome}, {quant}, {preco}");
-
-
-                    if (resp == 'N')
-                    {
-                        resp = 'N';
-                    }
-                    arquivo.Close();
-                }
-                arquivo.Close();
-
-            }
-            catch (IOException erro)
-            {
-                Console.Write("Erro na criaçao do arquivo: ");
-                Console.WriteLine(erro.Message);
-            }
+            Arquivo ar = new Arquivo();
+            ar.CriarArquivo();
         }
     }
 }
---
+++ File_FileInfo/utilidade/Arquivo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProdutoV2.utilidade
+{
+    class Arquivo
+    {
+        public string CriarArquivo()
+        {
+            try
+            {
+              StreamWriter arquivo;
+              string destino = @"C:\Produto.csv";
+              arquivo = File.CreateText(destino);
+              arquivo.Close();
+
+              char resp = 'S';
+              while (resp != 'N')
+              {
+                Console.Write($"Nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write($"Quantidade: ");
+                int quant = int.Parse(Console.ReadLine());
+                Console.Write($"Preço und do {nome}: ");
+                double preco = double.Parse(Console.ReadLine());
+                Console.Write($"Deseja cadastrar mais algum produto ? [S/N]");
+                resp = char.Parse(Console.ReadLine().ToUpper());
+
+                arquivo = File.AppendText(destino);
+                arquivo.WriteLine($"{nome}, {quant}, {preco}");
+                if (resp == 'N')
+                {
+                    resp = 'N';
+                }
+                arquivo.Close();
+              }
+            }
+            catch (IOException erro)
+            {
+                Console.Write("Erro na criaçao do arquivo: ");
+                Console.WriteLine(erro.Message);
+            }
+            return "Arquivo criado";
+        }
+        
+    }
+}
