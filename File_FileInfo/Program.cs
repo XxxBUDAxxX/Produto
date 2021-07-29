0f9011f307e596bc0fe2da1c88bb2041f3080664
John Wallas <johnnes.wallas@gmail.com>
2021-07-28 22:27:05 -0300

    criando o arquivo e colocando conteudo

---
+++ File_FileInfo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using ProdutoV2.utilidades;
+using System.Collections.Generic;
+
+namespace ProdutoV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter arquivo;
+                string destino = @"C:\Produto.csv";
+                arquivo = File.CreateText(destino);
+                arquivo.Close();
+
+                char resp = 'S';
+                while (resp != 'N')
+                {
+                    Console.Write($"Nome do produto: ");
+                    string nome = Console.ReadLine();
+                    Console.Write($"Quantidade: ");
+                    int quant = int.Parse(Console.ReadLine());
+                    Console.Write($"Preço und do {nome}: ");
+                    double preco = double.Parse(Console.ReadLine());
+                    Console.Write($"Deseja cadastrar mais algum produto ? [S/N]");
+                    resp = char.Parse(Console.ReadLine().ToUpper());
+
+                    arquivo = File.AppendText(destino);
+                    arquivo.WriteLine($"{nome}, {quant}, {preco}");
+
+
+                    if (resp == 'N')
+                    {
+                        resp = 'N';
+                    }
+                    arquivo.Close();
+                }
+                arquivo.Close();
+
+            }
+            catch (IOException erro)
+            {
+                Console.Write("Erro na criaçao do arquivo: ");
+                Console.WriteLine(erro.Message);
+            }
+        }
+    }
+}
